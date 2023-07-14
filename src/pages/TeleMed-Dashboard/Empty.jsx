c0f94246588ed56457a504246f2bfc2156bc42ae
Ekam-Bitt <ekambitt@gmail.com>
2023-07-14 20:36:50 +0530

    Fixed Book Doctor page of Telemedicine

--- src/pages/TeleMed-Dashboard/Empty.jsx
+++ src/pages/TeleMed-Dashboard/Empty.jsx
@@ -1,13 +1,18 @@
-import React from 'react'
+import React from "react";
 
 export default function Empty() {
   return (
     <div>
-      <div className="w-full h-[620px] flex flex-col justify-between items-center bg-[#FFE6E2]">
-        <p className=" text-2xl pt-8 pb-4 not-italic font-bold leading-[normal] font-family: Poppins;">
-            Empty
-          </p>
+      <div className="w-full h-[620px] flex flex-col justify-evenly items-center bg-[#FFE6E2]">
+        <p className=" text-2xl not-italic font-bold leading-[normal] font-family: Poppins;">
+          Why TeleMedicine is the Future ?
+        </p>
+        <div className=" w-[85%] h-[85%] rounded-xl flex justify-evenly items-center bg-white">
+          <div class="grid grid-cols-4 gap-4 bg-red-500">
+            <div className="bg-violet-1 rounded-xl flex flex-col justify-evenly items-center">a</div>
+          </div>
         </div>
+      </div>
     </div>
-  )
+  );
 }
