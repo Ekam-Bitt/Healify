0b54a6c859a6f56994540a4068f6f7784344395c
Ekam-Bitt <ekambitt@gmail.com>
2023-07-19 03:47:55 +0530

    Fixing Landing Page (1/n)

--- src/pages/Home/Type.jsx
+++ src/pages/Home/Type.jsx
@@ -2,21 +2,19 @@ import React from "react";
 import { useTypewriter } from "react-simple-typewriter";
 export default function Type() {
   const [word] = useTypewriter({
-    words: ["Your Life", "Your Money", "Your Time"],
+    words: [" Save Your Life", "Save Your Money", "Save Your Time"],
     loop: true,
     typeSpeed: 120,
     deleteSpeed: 80,
     cursorStyle: "|",
   });
   const [heading] = useTypewriter({
-    words: ["At Healify we can save"],
+    words: ["At Healify we can"],
   });
   return (
-    <div className=" appear justify-between pt-20 pl-10 mb-0  flex-col shrink-0 text-[#2C0472] text-[64px] not-italic font-normal leading-[normal]">
-      <h1 className="font-light text-7xl ">{heading}</h1>
-      <span className=" font-extrabold text-8xl flex w-[550px] flex-col shrink-0 text-[#2C0472] not-italic leading-[normal];">
-        {word}
-      </span>
+    <div className=" w-[55%] h-[156px] ml-16 mb-20">
+      <h1 className=" text-6xl font-light text-[#2C0472]">{heading}</h1>
+      <span className=" text-8xl font-extrabold text-[#2C0472]">{word}</span>
     </div>
   );
 }
