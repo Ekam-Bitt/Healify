1109a3cabd2ae36faa8963efcb96e4a8f4239365
Ekam-Bitt <ekambitt@gmail.com>
2023-07-19 11:18:25 +0530

    Fixed typewriter

--- src/pages/Home/Type.jsx
+++ src/pages/Home/Type.jsx
@@ -13,7 +13,7 @@ export default function Type() {
   });
   return (
     <div className=" w-[55%] h-[156px] ml-16 mb-20">
-      <h1 className=" text-6xl font-light text-[#2C0472]">{heading}</h1>
+      <h1 className=" text-6xl text-[#2C0472]">{heading}</h1>
       <span className=" text-8xl font-extrabold text-[#2C0472]">{word}</span>
     </div>
   );
